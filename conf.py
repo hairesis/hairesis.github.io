c59b2b57f09161d3e04780123a4ba4468cf69118
Andrea Baglioni <0x41ndrea@gmail.com>
2017-02-11 17:26:12 +0000

    Changed colur scheme

--- conf.py
+++ conf.py
@@ -39,7 +39,8 @@ html_favicon = '_static/tinkerer.ico'
 html_theme = 'flat'
 
 # Theme-specific options, see docs
-html_theme_options = {}
+
+html_theme_options = { 'accent_color': '#e74c3c'}
 
 # Link to RSS service like FeedBurner if any, otherwise feed is
 # linked directly
